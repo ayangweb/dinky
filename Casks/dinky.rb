3a8632eba96e4e9443bbe65f41551a0cbfb676cd
Derek J <127624400+heyderekj@users.noreply.github.com>
2026-05-03 10:31:24 -0500

    Bump to v2.9.1

--- Casks/dinky.rb
+++ Casks/dinky.rb
@@ -1,6 +1,6 @@
 cask "dinky" do
-  version "2.9.0"
-  sha256 "863dd7aef630895caf37f79c3e13a77b0ff6e253f6aa1b14e876f3bf08a1cc53"
+  version "2.9.1"
+  sha256 "a72f232c34501d7117b2c2424c289c0eb306477df867aba3bced3e92f32b7c16"
 
   url "https://github.com/heyderekj/dinky/releases/download/v#{version}/Dinky-#{version}.zip"
   name "Dinky"
